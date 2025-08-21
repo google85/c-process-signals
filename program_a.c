2c4c84ded8f249bda3f3d91d932272d8583b6ad5
Florin B <florin.buzec@gmail.com>
2025-08-21 19:00:52 +0300

    add handler for signal SIGINT (Ctrl+C)

--- program_a.c
+++ program_a.c
@@ -3,11 +3,18 @@
 #include <stdlib.h>
 #include <signal.h>
 
+void handler_sigint(int sig) {
+    printf("\nPlay audio meow.mp3!\n");
+}
+
 void handle_sigusr1(int sig) {
     printf("\nReceived SIGUSR1 signal! Continuing...\n");
 }
 
 int main(void) {
+    //Install the SIGINT handler [Ctrl+C]
+    signal(SIGINT, handler_sigint);
+
     //Install the SIGUSR1 handler
     signal(SIGUSR1, handle_sigusr1);
 
